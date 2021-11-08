20df1875fbf2c1ff228efd9eabf8a85edfb49e5f
Jose1320114014 <92335524+Jose1320114014@users.noreply.github.com>
2021-11-07 22:57:20 -0600

    Add files via upload

--- capital toys/correo.php
+++ capital toys/correo.php
@@ -1,10 +1,8 @@
 <?php
-
-require("enlaze.php");
+require ("servidor.php");
 require("class.phpmailer.php");
+require("class.smtp.php");
 require ("postman.php");
-$mail = new PHPMailer();
-
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -17,16 +15,16 @@ try {
     //Server settings
     $mail->SMTPDebug = 0;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.outlook.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'correo que quieren que mande los mails';                     //SMTP username
-    $mail->Password   = 'contraseña del correo';                               //SMTP password
+    $mail->Username   = 'Capital_Toys@outlook.com';                     //SMTP username
+    $mail->Password   = 'ponle_contrasella';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
 
     //Recipients
-    $mail->setFrom('correo que quieren que mande los mails', 'nombre de su empresa');
-    $mail->addAddress("$correo", "$nombre");     //Add a recipient
+    $mail->setFrom('Capital_Toys@outlook.com', 'Capital Toys');
+    $mail->addAddress("$email", "$nombre");     //Add a recipient
 
 
 
@@ -42,6 +40,4 @@ try {
 } catch (Exception $e) {
     echo "Isra la cagaste en algo: {$mail->ErrorInfo}";
 }
-
-
 ?>
--- capital toys/postman.php
+++ capital toys/postman.php
@@ -1,5 +1,7 @@
 <?php
+
 require ("servidor.php");
+
 $nombre = "";
 $apellidos = "";
 $email = "";
@@ -43,4 +45,41 @@ if (mysqli_query($conn, $sql)){
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 mysqli_close($conn);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require ("vendor/autoload.php");
+
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.outlook.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'Capital_Toys@outlook.com';                     //SMTP username
+    $mail->Password   = 'ponle_contrasella';                               //SMTP password
+    $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                  //TCP port to connect to; use 587 if you have set SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS
+
+    //Recipients
+    $mail->setFrom('Capital_Toys@outlook.com', 'Capital Toys');
+    $mail->addAddress("$email", "$nombre");     //Add a recipient
+
+
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Registro de cuenta exitoso';
+    $mail->Body    = 'Estimado usuario se le informa que el registro de su cuenta ha sido exitoso';
+
+
+    $mail->send();
+    echo 'El mensaje se envio correctamente';
+} catch (Exception $e) {
+    echo "Error: {$mail->ErrorInfo}";
+}
 ?>
